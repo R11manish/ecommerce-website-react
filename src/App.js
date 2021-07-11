76fe967102df75e30ed5edc6abce653dfc272fc9
Manish Rawat <gihub.jmtss@gmail.com>
2021-07-11 13:04:14 +0530

    setup basic structure and done some modfication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div className='App'>
+      <HomePage />
     </div>
   );
 }
