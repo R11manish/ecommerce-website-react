f4f8d75f27320885de56f7c60e03acf7a0b84d3e
Manish Rawat <github.jmtss@gmail.com>
2021-07-15 09:47:46 +0530

    including form section in signin page

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
 
 class SignIn extends Component {
   constructor(props) {
@@ -26,22 +27,22 @@ class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name='email'
             type='email'
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='email'
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name='password'
             type='password'
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label='password'
             required
           />
-          <label>Password</label>
           <input type='submit' value='Submit Form' />
         </form>
       </div>
