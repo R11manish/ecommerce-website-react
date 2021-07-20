a997be6d39df6dac027cf21b3f36607fe3a3436e
Manish Rawat <github.jmtss@gmail.com>
2021-07-20 18:56:18 +0530

    finishing signin handle function

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/cutom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utlis';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utlis';
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -13,9 +13,15 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
