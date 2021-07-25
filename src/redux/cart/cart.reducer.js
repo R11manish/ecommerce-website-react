3576e65af10970ac620463fa943c818935c6ce49
Manish Rawat <github.jmtss@gmail.com>
2021-07-25 17:59:34 +0530

    FIX : small bug in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -17,7 +17,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemTOCart(state.cartItems, action, payload),
+        cartItems: addItemTOCart(state.cartItems, action.payload),
       };
 
     default:
