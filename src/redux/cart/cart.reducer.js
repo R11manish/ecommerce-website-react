e71a08c15aa267ca24c25ced1d35ca621c0f633d
Manish Rawat <github.jmtss@gmail.com>
2021-07-25 10:13:28 +0530

    refactor the code and implemented dispatch actions fot cart items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -12,6 +12,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden,
       };
 
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+
     default:
       return state;
   }
