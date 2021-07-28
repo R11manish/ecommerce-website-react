815c286683313bcbd3922524df0a86c58e441814
Manish Rawat <github.jmtss@gmail.com>
2021-07-28 15:53:31 +0530

    created  cartPage and added route for it in app.js

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router';
 
 import CustomButton from '../custom-button/cutom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -18,7 +19,9 @@ const CartDropdown = ({ cartItems }) => (
         <span className='empty-message'>your cart is empty</span>
       )}
     </div>
-    <CustomButton>Go To CheckOut</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      Go To CheckOut
+    </CustomButton>
   </div>
 );
 
@@ -26,4 +29,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
