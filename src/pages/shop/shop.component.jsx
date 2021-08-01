c7b4a8b19b32c3dfdd812858b422891f8e6fbdcc
Manish Rawat <github.jmtss@gmail.com>
2021-08-01 23:15:53 +0530

    Adding category section for different collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
+import CategoryPage from '../category/category.component';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
   </div>
 );
 
