a481b522ac8d26c43930caaff846fe3db572d4c5
Manish Rawat <github.jmtss@gmail.com>
2021-08-06 00:00:18 +0530

    refactor code with as property

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,7 +18,6 @@ import {
   LogoContainer,
   OptionsContainer,
   OptionLink,
-  OptionDiv,
 } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
@@ -31,7 +30,9 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to='/shop'>SHOP</OptionLink>
       <OptionLink to='/contact'>CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+        <OptionLink as='div' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
       ) : (
         <OptionLink to='/signin'>SIGN IN</OptionLink>
       )}
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,10 +1,5 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
-const optionContainerStyles = css`
-  padding: 10px 15px;
-  cursor: pointer;
-`;
 
 export const HeaderContainer = styled.div`
   height: 70px;
@@ -29,9 +24,6 @@ export const OptionsContainer = styled.div`
 `;
 
 export const OptionLink = styled(Link)`
-  ${optionContainerStyles}
-`;
-
-export const OptionDiv = styled.div`
-  ${optionContainerStyles}
+  padding: 10px 15px;
+  cursor: pointer;
 `;
