e26074ced3b86118cbf3d0146e84d386c646eda6
Manish Rawat <github.jmtss@gmail.com>
2021-08-11 10:55:03 +0530

    shifted shop data to firebase and now fetching data from firebase

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
+import {
+  firestore,
+  convertCollectionsSnapShotToMap,
+} from '../../firebase/firebase.utlis';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async (snapShot) => {
+      convertCollectionsSnapShotToMap(snapShot);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
