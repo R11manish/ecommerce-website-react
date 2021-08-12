35d326faaba0309e2dea4af10c894467cd303972
Manish Rawat <github.jmtss@gmail.com>
2021-08-12 12:53:34 +0530

    implemented withspinner component with shop.component page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,8 +8,15 @@ import {
 } from '../../firebase/firebase.utlis';
 import CollectionPage from '../collection/collection.component';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
   unsubscribeFromSnapshot = null;
   componentDidMount() {
     const { updateCollections } = this.props;
@@ -18,19 +25,29 @@ class ShopPage extends React.Component {
       async (snapShot) => {
         const collectionsMap = convertCollectionsSnapShotToMap(snapShot);
         updateCollections(collectionsMap);
+        this.setState({ loading: false });
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
 
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
@@ -39,7 +56,7 @@ class ShopPage extends React.Component {
 
 const mapDispatchToProps = (dispatch) => ({
   updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap))
+    dispatch(updateCollections(collectionsMap)),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
