4c5e359d4ae6ecf3402972d355eae579cfb38cf9
Manish Rawat <github.jmtss@gmail.com>
2021-08-15 17:07:21 +0530

    fetching firestore data using redux-saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,8 +1,29 @@
-import { takeEvery } from '@redux-saga/core/effects';
+import { takeEvery, call, put } from '@redux-saga/core/effects';
 import ShopActionTypes from './shop.types';
+import {
+  firestore,
+  convertCollectionsSnapShotToMap,
+} from '../../firebase/firebase.utlis';
+
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shop.actions';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('i am fired');
+  try {
+    yield console.log('i am fired');
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapShotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
